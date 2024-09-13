f37edd3c977076d24816bdf047ba2d1702119663
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-09-13 13:17:31 +0800

    String Manipulation

---
+++ string.js
@@ -0,0 +1,20 @@
+let favActorFirstName = "Tom";
+let favActorLastName = "Holland";
+//Concatenate
+let fullName = favActorFirstName + ' ' + favActorLastName;
+
+ //UPPER AND LOWER CASE
+let uppercase = "tom holland";
+let lowercase =  "GINGER"
+
+//MESSAGE
+let message = "My favorite actor is " + fullName.toUpperCase();
+message += " His best show is Silicon Valley";
+
+//OUTPUT
+console.log("Favorite Actor First Name:", favActorFirstName);
+console.log("Favorite Actor Last Name:", favActorLastName);
+console.log("Full Name:", fullName);
+console.log("Uppercase Name:", uppercase.toUpperCase());
+console.log("Lowercase Name:", lowercase.toLowerCase());
+console.log("Message:", message);
